f934d0e4eaeae693cb25923ad6e1b5096e151f5e
Delonely4 <ildar199718@icloud.com>
2025-10-24 19:33:28 +0300

    cors_updated

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,48 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/routes.js';
-import bookRoutes from './routes/bookRoutes.js';
-import chapterRoutes from './routes/chapterRoutes.js';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/routes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+import chapterRoutes from "./routes/chapterRoutes.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "MangaFlow!",
-        version: "1.0.0",
-        endpoints: {
-            register: "POST /api/auth/register",
-            login: "POST /api/auth/login",
-            protected: "GET /api/auth/protected",
+app.get("/", (req, res) => {
+  res.json({
+    message: "MangaFlow!",
+    version: "1.0.0",
+    endpoints: {
+      register: "POST /api/auth/register",
+      login: "POST /api/auth/login",
+      protected: "GET /api/auth/protected",
 
-            getAllBooks: "GET /api/books",
-            getBook: "GET /api/books/:id",
-            createBook: "POST /api/books",
-            updateBook: "PUT /api/books/:id",
-            deleteBook: "DELETE /api/books/:id",
+      getAllBooks: "GET /api/books",
+      getBook: "GET /api/books/:id",
+      createBook: "POST /api/books",
+      updateBook: "PUT /api/books/:id",
+      deleteBook: "DELETE /api/books/:id",
 
-            getChapters: "GET /api/chapters/book/:bookId",
-            createChapter: "POST /api/chapters",
-            createMultipleChapters: "POST /api/chapters/bulk",
-            toggleChapterRead: "POST /api/chapters/:bookId/:number/read",
-            updateChapter: "PUT /api/chapters/:id",
-            deleteChapter: "DELETE /api/chapters/:id"
-        }
-    });
+      getChapters: "GET /api/chapters/book/:bookId",
+      createChapter: "POST /api/chapters",
+      createMultipleChapters: "POST /api/chapters/bulk",
+      toggleChapterRead: "POST /api/chapters/:bookId/:number/read",
+      updateChapter: "PUT /api/chapters/:id",
+      deleteChapter: "DELETE /api/chapters/:id",
+    },
+  });
 });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/books', bookRoutes);
-app.use('/api/chapters', chapterRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/chapters", chapterRoutes);
 
 export default app;
